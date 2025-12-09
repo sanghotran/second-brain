71b2ec21335e48fac9c76224ba5101320ab492f8
Trần Minh Sang <sangtranminh@thegioididong.com>
2025-12-09 14:47:56 +0700

    register plugin

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -7,8 +7,17 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init()) // <--- BỔ SUNG DÒNG NÀY
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
